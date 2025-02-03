f1fc080fbb09cca7c379e517abbebdd1e810824c
12hellyeah12 <113688239+12hellyeah12@users.noreply.github.com>
2025-02-03 11:32:09 +0100

    Finish project 08 calculator #daumenhoch

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,34 @@
-const add = function() {
-	
+// to run test in terminal
+//npm test calculator.spec.js
+
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((acc, el) => acc += el, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acc, el) => acc *= el);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num <= 0){
+    return 1;
+  } 
+  return num * factorial(num - 1);
 };
+
+console.log(factorial(5));
 
 // Do not edit below this line
 module.exports = {
