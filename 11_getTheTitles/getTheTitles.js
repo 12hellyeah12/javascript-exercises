b12626ac377108434ef5854db63325fe7769844e
12hellyeah12 <113688239+12hellyeah12@users.noreply.github.com>
2025-02-04 15:12:53 +0100

    Finish project

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
+//npm test getTheTitles.spec.js
+/*
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ];
+  */
 
+const getTheTitles = function(array) {
+    return titles = array.map(object => object.title);
 };
+
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
