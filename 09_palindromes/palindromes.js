1e8d2cac5944d094134b7edabaaefcc35b41feb7
12hellyeah12 <113688239+12hellyeah12@users.noreply.github.com>
2025-02-04 09:57:48 +0100

    Finish 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,39 @@
-const palindromes = function () {
+//npm test palindromes.spec.js
 
+const palindromes = function (palindrome) {
+/* 
+Take palindrome as the input convert it to a lower case
+string
+In order to get rid of punctuation and white space with
+reduce use split to convert "palindrome" into an array
+*/
+const cleanArray = 
+palindrome.toLowerCase().
+split("").
+reduce((accumulator, char) => {
+    if(
+        char == "."
+        || char == "!"
+        || char == "?"
+        || char == ","
+        || char == " "
+    ){
+        accumulator.push("");
+    } else {
+        accumulator.push(char);
+    }
+    return accumulator;
+}, []);
+
+// Read clean Array in reversed order and rejoin it into 
+// a string
+const reverseInput = 
+cleanArray.toReversed().
+join("");
+
+const input = cleanArray.join("");
+
+return reverseInput === input ? true : false;
 };
 
 // Do not edit below this line
